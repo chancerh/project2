df8d2aa6b1147d1be88ed3d9be543df961aee296
Chance <chancerh@gmail.com>
2015-04-19 16:47:56 -0700

    Add database schema

--- tournament.sql
+++ tournament.sql
@@ -7,3 +7,39 @@
 -- these lines here.
 
 
+-- Table of players. One row per player.
+CREATE TABLE players (
+       id SERIAL PRIMARY KEY,
+       name TEXT
+);
+
+-- table of matches. 
+CREATE TABLE matches (
+       match SERIAL PRIMARY KEY,
+       player1 REFERENCES players(id),
+       player2 REFERENCES players(id),
+       winner REFERENCES players(id)
+);     
+       
+-- Views for the following:
+-- * Number of matches each player has played
+-- * The number of wins for each player
+-- * The player standings.
+
+-- Number of matches. In Swiss style tournaments, doesn't everybody play in every round?
+
+-- * The number of wins for each player
+CREATE OR REPLACE VIEW playerWins AS
+       SELECT name, id, COUNT(winner) as numWins
+       FROM players LEFT JOIN matches
+       ON plyers.id = matches.winner
+       GROUP BY players.id
+;
+
+
+CREATE OR REPLACE VIEW playerStanding AS
+       SELECT * from playerWins
+       ORDER BY numWins
+;
+       
+       
